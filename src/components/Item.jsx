d7843bd7a30941ba5d972d6965da4e412988cea3
FacundoPandol <facundopandol90@gmail.com>
2024-02-25 08:49:13 -0300

    descripcion de las cards

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,7 +6,8 @@ export const Item = ({ product }) => {
     return (
         <div className="max-w-xs mx-auto bg-gray-200 p-3 rounded-md shadow-md border-4 my-4 card">
             <img src={`../img/${product.img}`} alt={`Imagen de ${product.title}`} className='product-image' />
-            <h2 className="text-xs font-bold mb-2">{product.title} {product.description}</h2>
+            <h2 className="text-xs font-bold mb-2">{product.title} </h2>
+            <p className="text-xs font-bold xs-1">{product.description}</p>
             <p className="text-gray-700 mb-2">Stock: {product.stock}</p>
             <span className="text-xl mb-2">Precio: ${product.price}</span>
             <ItemCount stock={product.stock} />
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,25 +1,26 @@
 import { useCounter } from "../hooks/useCounter";
+
 export const ItemCount = ({ stock }) => {
-    const { count, increment, decrement } = useCounter(1, stock, 1)
+    const { count, increment, decrement } = useCounter(1, stock, 1);
 
     const handleAddToCart = () => {
         console.log("Producto agregado al carrito");
     };
 
     return (
-        <div className="container mx-auto mt-8">
+        <div className="container mx-auto mt-8 flex flex-col items-center justify center">
             <div className="flex items-center justify-center space-x-4">
-                <button className="hover:bg-blue-700 py-1 px-4 rounded btn btn-outline-success" onClick={decrement}>
+                <button className="text-sm hover:bg-blue-700 py-1 px-2 rounded btn btn-outline-success" onClick={decrement}>
                     -
                 </button>
-                <span className="text-4x4 font-bold">{count}</span>
-                <button className="hover:bg-blue-700 py-1 px-4 rounded btn btn-outline-success" onClick={increment}>
+                <span className="text-4xl font-bold">{count}</span>
+                <button className="text-sm hover:bg-blue-700 py-1 px-2 rounded btn btn-outline-success" onClick={increment}>
                     +
                 </button>
-                <button className="hover:bg-blue-700 py-1 px-4 rounded btn btn-outline-success" onClick={handleAddToCart}>
-                    Agregar al carrito
-                </button>
             </div>
+            <button className="mt-2 text-sm hover:bg-blue-700 py-1 px-4 rounded btn btn-outline-success" onClick={handleAddToCart}>
+                Agregar al carrito
+            </button>
         </div>
     );
 };
