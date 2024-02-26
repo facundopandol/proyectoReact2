d95d956391854d1346f847ae6da459524c70c039
FacundoPandol <facundopandol90@gmail.com>
2024-02-26 11:05:41 -0300

    cambio de logo

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -14,13 +14,13 @@ export const ItemDetail = ({ item }) => {
         <div className="fixed w-full h-full flex items-center justify-center z-50">
             <div className= "bg-black bg-opacity-50">
                 <div className="bg-black p-8 rounded-lg">
-                    <Link to={'/'}>
-                        <button className="absolute top-4 right-4 text-black-700 hover:text-black-900">X</button>
+                <Link to={'/'}>
+                        <button className="text-black-700 hover:text-black-900">X</button>
                     </Link>
-                    <div className=" w-full h-full flex items-center justify-center text-center item-card">
+                    <div className="w-full h-full flex items-center justify-center text-center item-card">
                     <img className="w-full h-64 object-cover mb-6" src={`../img/${item.img}`} alt={`Imagen de ${item.title}`} />
                         <h2 className="text-2xl font-bold mb-2">{item.title}</h2>
-                        <p className="text-gray-700 mb-2 font-semibold">Descripción: {item.description}</p>
+                        <p className="text-gray-700 mb-2 font-semibold">{item.description}</p>
                         <p className="text-gray-700 mb-2 font-semibold">Stock: {item.stock}</p>
                         <p className="text-gray-700 mb-2 font-semibold">Precio: ${item.price}</p>
                         <div className="container mx-auto mt-8">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,9 @@ export const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
       <Link to={'/'} className="nav-link text-white">
-      <div className="navbar-brand">LOGO</div>
+        <img src="../public/img/logo.png" alt="Logo" className="navbar-brand" />
+     {// <div className="navbar-brand">LOGO</div>
+     }
                 </Link>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
